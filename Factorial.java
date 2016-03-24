19c282a5746ba0c43fff4a6de888ca8becdafe60
Eric.GTW <857212763@qq.com>
2016-03-24 13:17:06 +0800

    add

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+public class Factorial {
+    public static void main(String[]args) {
+       for(int i = 1; i <= 20; i ++) {
+           System.out.println(sum(i));
+       }
+    }
+
+    public static long sum(long n) {
+        long sum = 0 ;
+        long a0 = 1;
+        long s0 = 0;
+        for(int i = 1; i <= n ;i ++) {
+            sum = s0 + a0 * i;
+            a0 = a0 * i;
+            s0 = sum;
+        }
+        return sum;
+    }
+}
